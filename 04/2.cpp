977901283228750b8e0b744a874c7c916888f8dd
Laurent Georget <laurent.georget@meteo-concept.fr>
2022-12-04 22:42:23 +0100

    Solve day 4

---
+++ 04/2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+int main()
+{
+	int total = 0;
+
+	int min1, max1, min2, max2;
+	std::cin >> min1;
+	std::cin.ignore(1);
+	std::cin >> max1;
+	std::cin.ignore(1);
+	std::cin >> min2;
+	std::cin.ignore(1);
+	std::cin >> max2;
+
+	while (std::cin)
+	{
+		if ((min2 >= min1 && min2 <= max1) ||
+		    (min1 >= min2 && min1 <= max2)) {
+			total++;
+		}
+
+		std::cin >> min1;
+		std::cin.ignore(1);
+		std::cin >> max1;
+		std::cin.ignore(1);
+		std::cin >> min2;
+		std::cin.ignore(1);
+		std::cin >> max2;
+	}
+
+	std::cout << total << std::endl;
+}
